ebc4c753481d46503ed8747eba421d05b0195e31
Kevin <kevinjuarez@gmail.com>
2025-01-01 23:35:56 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Variable", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
